cd770da7e4d5658e4c425ef7c186d02d86a09a2f
DeepakChaurasia30 <chaurasiaanita391@gmail.com>
2021-04-23 12:34:38 +0530

    underclock_cpu

--- device.mk
+++ device.mk
@@ -343,7 +343,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
      ro.vendor.extension_library=libqti-perfd-client.so \
-     ro.min_freq_0=800000 \
+     ro.min_freq_0=200000 \
+     ro.min_freq_4=200000 \
      ro.core_ctl_min_cpu=0 \
      ro.core_ctl_max_cpu=2
 
